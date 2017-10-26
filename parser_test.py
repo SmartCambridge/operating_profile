f117e2b98d92df30884e1b625a90594ec32c9049
Jon Warbrick <jw35@cam.ac.uk>
2017-10-26 17:07:45 +0100

    Switch output format to be JSON
    xmldict created OrderedDicts which pprint renders confusingly

--- parser_test.py
+++ parser_test.py
@@ -3,7 +3,9 @@
 import xmltodict
 import pprint
 import sys
+import json
 
 with open(sys.argv[1], 'rb') as xml_file:
     content = xmltodict.parse(xml_file)
-    print(pprint.pprint(content))
+    #print(pprint.pprint(content))
+    print(json.dumps(content, indent=2))
